3089bf4936abcb82c2d47b51fda58ed9cf6d27b5
devlumba <ilovesylphy@gmail.com>
2024-03-28 00:23:17 +0300

    2nd commit, changed main.py and added passenger_wsgi.py

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route("/")
+@application.route("/")
 def hello_world():
     return render_template("base.html")
 
 
-@app.route("/about")
+@application.route("/about")
 def about():
     return render_template("about.html", title="About")
 
 
-@app.route("/characters")
+@application.route("/characters")
 def characters():
     pass
 
 
-@app.route("/персонажи")
-def characters_ru():
-    return "Персонажи"
-
-
-@app.route("/staff")
+@application.route("/staff")
 def staff():
     pass
