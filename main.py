18959c2b2e6bb6a8d2d27a547db0ada88cabcd9f
devlumba <ilovesylphy@gmail.com>
2024-03-31 01:55:25 +0300

    4th commit, app is finished

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import werkzeug.exceptions
 from flask import Flask, render_template
 
 application = Flask(__name__)
-
+application.config['TRAP_HTTP_EXCEPTIONS']=True
 
 @application.route("/")
 def home():
@@ -10,17 +11,31 @@ def home():
 
 @application.route("/about/")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="Новелла")
 
 
 @application.route("/characters/")
 def characters():
-    return render_template("about.html")
+    return render_template("characters.html", title="Персонажи")
 
 
 @application.route("/download/")
 def download():
-    return render_template("download.html")
+    return render_template("download.html", title="Скачать")
+
+
+@application.route("/staff/")
+def staff():
+    return render_template("staff.html", title='Создатели')
+
+
+@application.errorhandler(Exception)
+def bad_request(e):
+    try:
+        if e.code == 404:
+            return render_template("404.html", title="Ошибка 404!"), 404
+    except:
+        pass
 
 
 if __name__ == "__main__":
